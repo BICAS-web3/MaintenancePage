44520417d2b688f6c287a75f90ea8d2196383b06
Rishat2000 <risatosmonov@gmail.com>
2023-10-10 03:22:00 +0600

    created new page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import MainRoutes from './MainRoutes';
+
+function App() {
+	return (
+		<>
+			<MainRoutes />
+		</>
+	);
+}
+
+export default App;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "../styles/HomePage.module.scss";
+import centerImage from "../img/toolsIcon.png";
+import circle from "../img/circle.png";
+import logo from "../img/logo.png";
+
+const HomePage = () => {
+	return (
+		<>
+			<div className={styles.main}>
+				<div className={styles.main_image}>
+					<div className={styles.image}>
+						<div className={styles.image_first}>
+							<img
+								src={centerImage}
+								alt="image"
+							/>
+							<div className={styles.image_last}>
+								<img
+									src={circle}
+									alt="image"
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className={styles.main_text}>
+					<div className={styles.text_title}>
+						Sorry, we’re down for maintenance
+					</div>
+					<div className={styles.text_sub_title}>Please check back later!</div>
+				</div>
+			</div>
+			<a
+				href="#"
+				className={styles.logo}
+			>
+				<img
+					src={logo}
+					alt="logo"
+				/>
+			</a>
+		</>
+	);
+};
+
+export default HomePage;
